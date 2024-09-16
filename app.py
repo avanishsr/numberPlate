7f6266c3da44491cbaddd0e8ed61bbfabdddaab0
avanishsr <147104174+avanishsr@users.noreply.github.com>
2024-09-17 03:04:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from PIL import Image
 app = Flask(__name__)
 
 # Load YOLOv8 model (ensure the best.pt is in the same folder as this script)
-model = YOLO("best.pt")
+model = YOLO(os.path.join(os.path.dirname(__file__), "best.pt"))
+
 
 # Create EasyOCR reader (for OCR)
 reader = easyocr.Reader(['en'])
