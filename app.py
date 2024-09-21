2f90db536f1ecda9ac2a85dbe1a615f186eac9f2
Avanish <avanishsrivastava.as@gmail.com>
2024-09-22 02:29:50 +0530

    first commit

--- app.py
+++ app.py
@@ -1,22 +1,16 @@
-import os
-import cv2
-import torch
 import base64
-import re
+import os
 from flask import Flask, request, jsonify, render_template
 from ultralytics import YOLO
-import easyocr
 from io import BytesIO
 from PIL import Image
+import cv2
 
 # Initialize Flask app
 app = Flask(__name__)
 
 # Load YOLOv8 model (ensure the best.pt is in the same folder as this script)
-model = YOLO("weights/best.pt")
-
-# Create EasyOCR reader (for OCR)
-reader = easyocr.Reader(['en'])
+model = YOLO("best.pt")
 
 # Ensure the output folder exists
 if not os.path.exists("static/output"):
@@ -98,28 +92,8 @@ def detect_best_plate_from_video():
         cropped_image_pil.save(buffered, format="JPEG")
         cropped_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-        # Perform OCR on the cropped license plate
-        ocr_result = reader.readtext(cropped_plate, detail=0)  # Extract only text
-
-        # Convert OCR result to a single string, uppercase, no spaces, remove special characters
-        if ocr_result:
-            ocr_text = ''.join(ocr_result).upper()  # Join and convert to uppercase
-            ocr_text_clean = re.sub(r'[^A-Z0-9]', '', ocr_text)  # Remove special characters
-
-            # Apply additional logic:
-            # 1. If the first two characters contain '0', replace with 'O'
-            if len(ocr_text_clean) >= 2:
-                if '0' in ocr_text_clean[:2]:
-                    ocr_text_clean = ocr_text_clean[:2].replace('0', 'O') + ocr_text_clean[2:]
-
-            # 2. If the third and fourth characters contain 'O', replace with '0'
-            if len(ocr_text_clean) >= 4:
-                if 'O' in ocr_text_clean[2:4]:
-                    ocr_text_clean = ocr_text_clean[:2] + ocr_text_clean[2:4].replace('O', '0') + ocr_text_clean[4:]
-
-        # Return the cleaned and adjusted OCR result and the corresponding cropped image
+        # Return the base64 image of the cropped license plate
         return jsonify({
-            'detected_text': ocr_text_clean,  # Return the modified OCR result
             'cropped_image_base64': cropped_base64
         })
     else:
